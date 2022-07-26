0e5042adbb1f214ddc3170f01257a45ca7c143eb
AndreaHuP <85973046+AndreaHuP@users.noreply.github.com>
2022-07-26 13:48:31 -0500

    Add files via upload

---
+++ Taller modulo 3.R
@@ -0,0 +1,19 @@
+##ANDREA HUERTAS PEDREROS Y TATIANA ESCALANTE
+
+library(tidyverse)
+
+Spotify <- read.csv("songs_normalize.csv")
+
+##PUNTO 1.
+grafico_1 <- ggplot(Spotify, aes(x = danceability, y = valence)) +
+  geom_point(color = "blue", alpha = 0.3)
+
+##PUNTO 2.
+grafico_2 <- ggplot(Spotify, aes(x = as.factor(mode), y = energy, color = mode)) +
+  geom_boxplot(show.legend = FALSE)
+
+##PUNTO 3.
+install.packages("patchwork")
+library(patchwork)
+
+grafico_1 + grafico_2
